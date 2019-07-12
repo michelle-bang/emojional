b89dcd1d6c263fb726b5174045de34222a547904
Michelle <apple@Apples-MacBook-Pro-98.local>
2019-07-12 12:44:40 -0500

    Complete Iteration 3

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -15,19 +15,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    let customMessages = ["sun" : ["good morning", "go back to sleep", "rise and shine"], "moon" : ["good night", "why are you still awake?", "sleep tight"]]
+    
+    let emojis = ["🌞" : "sun", "🌚" : "moon"]
+    
     @IBAction func showMessage(sender: UIButton){
-        let emojis = ["🌞": "sun", "🌚": "moon"]
-        
 
         let selectedEmotion = sender.titleLabel?.text
         
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int.random(in: 0...2)]
+        
         //a message pops up when a user clicks a button
-        let alertController = UIAlertController(title: "Hey!", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "Hey!", message: customMessages[emojis[selectedEmotion!]!]?[Int.random(in: 0...2)], preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
             
         present(alertController, animated: true, completion: nil)
     }
+    
+    
+    
 }
 
 
